07f81f96d821b96a6ad925378e58669952dba257
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-11-02 23:02:55 +0530

    vite changed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Set base to ensure relative paths for assets in production
+  base: './',
 })
