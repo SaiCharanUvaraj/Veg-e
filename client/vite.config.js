b35ea67d6b1bc1ace3985ea7d60a657868858029
SaiCharanUvaraj <saicharanuvi01@gmail.com>
2024-11-02 22:25:31 +0530

    rearranged indev and vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Set base to ensure relative paths for assets in production
 })
